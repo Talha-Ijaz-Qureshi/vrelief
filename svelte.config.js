50ae080b108551a16788ba7ddd5b5b386c927feb
Talha Ijaz Qureshi <mrwcggr121@gmail.com>
2025-04-18 19:53:22 +0400

    woo

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,25 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null // or 'index.html' if using SPA fallback
+		}),
+		paths: {
+			base: dev ? '' : '/vrelief'
+		},
+		prerender: {
+			handleHttpError: 'warn', // 👈 prevents crash on 404 during build
+			entries: ['*'] // 👈 prerender all reachable routes (or specify exact pages)
+		}
 	}
 };
 
