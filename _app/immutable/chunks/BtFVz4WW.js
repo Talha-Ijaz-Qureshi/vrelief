8e5b8000404699b9f76b6d9e5a8359cfb584cd28
Talha Ijaz Qureshi <mrwcggr121@gmail.com>
2025-04-28 22:07:01 +0400

    Updates

---
+++ _app/immutable/chunks/BtFVz4WW.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_10ooxrs)==null?void 0:s.base)??"/vrelief";var e;const o=((e=globalThis.__sveltekit_10ooxrs)==null?void 0:e.assets)??a;export{o as a,a as b};
--- _app/immutable/entry/start.D2Ldl-xK.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CmW0fRiG.js";export{o as load_css,r as start};
